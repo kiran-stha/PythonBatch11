7dacc1e72607fc4dc2e5a025af85204f6f0614cd
kiran <shrestakeyran3@gamil.com>
2023-02-03 13:26:18 +0545

    Project 1 : Class and Object

---
+++ Shapes.py
@@ -0,0 +1,37 @@
+class shape:
+    name = "Geometric Shapes" #class variable
+
+    def print_name(self):
+        print(self.name)
+
+    def get_area(self):
+        return None
+class Rectangle(shape):
+    name = 'Rectangle'
+
+    def __init__(self, width,height):
+        self.width = width
+        self.height = height
+    def get_area(self):
+        return self.width*self.height
+    def get_perimeter(self):
+        return 2*(self.width+self.height)
+
+class Triangle(shape):
+    name = 'Triangle'
+
+    def __init__(self, width,height):
+        self.width = width
+        self.height = height
+    def get_area(self):
+        return (self.width*self.height)/2
+
+class Circle(shape):
+    name = 'Circle'
+    PI = 22/7
+
+    def __init__(self, radius):
+        self.radius = radius
+        
+    def get_area(self):
+        return self.PI*self.width*self.height**2
